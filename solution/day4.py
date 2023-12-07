1af8a6011f88e47a61934f9f5df66645e1cf8f3b
J. Ramon Claramunt <jr.claramunt@gmail.com>
2023-12-07 01:27:03 +0100

    Small refactor Day 4

--- solution/day4.py
+++ solution/day4.py
@@ -12,7 +12,7 @@ class Day4(Day):
         for card in args[0]:
             card_data = re.match(regex, card.strip()).groups()
             card_id = card_data[0]
-            
+
             self.cards[card_id] = {}
             self.cards[card_id]['winning_numbers'] = list(map(lambda x: int(x), re.split(r' +', card_data[1].strip())))
             self.cards[card_id]['played_numbers'] = list(map(lambda x: int(x), re.split(r' +', card_data[2].strip())))
@@ -20,26 +20,24 @@ class Day4(Day):
     def part1(self):
         total = 0
         for card_data in self.cards.values():
-            matching_numbers = list(set(card_data['winning_numbers']) & set(card_data['played_numbers']))
-            total += floor(pow(2, len(matching_numbers) - 1))
+            n_of_matching_numbers = self.matching_numbers(card_data)
+            total += floor(pow(2, n_of_matching_numbers - 1))
 
         return total
 
     def part2(self):
+        n_of_instances = {}
         for card_id in self.cards.keys():
-            self.cards[card_id]['instances'] = 1
+            n_of_instances[card_id] = 1
 
         for card_id, card_data in self.cards.items():
-            matching_numbers = list(set(card_data['winning_numbers']) & set(card_data['played_numbers']))
-            if len(matching_numbers) > 0:
-                for i in range(int(card_id) + 1, int(card_id) + len(matching_numbers) + 1):
-                    self.cards[str(i)]['instances'] += self.cards[card_id]['instances']
+            n_of_matching_numbers = self.matching_numbers(card_data)
+            if n_of_matching_numbers > 0:
+                for i in range(int(card_id) + 1, int(card_id) + n_of_matching_numbers + 1):
+                    n_of_instances[str(i)] += n_of_instances[card_id]
 
-        total = 0
-        for a in self.cards.values():
-            total += a['instances']
+        return sum(n_of_instances.values())
 
-        return total
-
-
-
+    @staticmethod
+    def matching_numbers(card_data):
+        return len(list(set(card_data['winning_numbers']) & set(card_data['played_numbers'])))
